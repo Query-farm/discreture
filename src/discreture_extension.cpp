684c472870658eda41232a20f74f4b36d6286005
Rusty Conover <rusty@conover.me>
2025-08-12 07:59:51 -0400

    fix: make permutations multi threaded, and add array output

--- src/discreture_extension.cpp
+++ src/discreture_extension.cpp
@@ -25,7 +25,8 @@ struct CombBindData : public TableFunctionData {
 
 struct PermBindData : public TableFunctionData {
 	int n;
-	PermBindData(int n_p) : n(n_p) {
+	bool return_array;
+	PermBindData(int n_p, bool return_array_p) : n(n_p), return_array(return_array_p) {
 	}
 };
 
@@ -98,11 +99,68 @@ struct CombLocalState : public LocalTableFunctionState {
 };
 
 struct PermGlobalState : public GlobalTableFunctionState {
-	discreture::Permutations<int> perm;
-	discreture::Permutations<int>::iterator it;
-	idx_t n;
+public:
+	const discreture::Permutations<int> perm;
+	const idx_t n;
+	atomic<int32_t> completed;
 
-	PermGlobalState(int n_val) : perm(n_val), it(perm.begin()), n(n_val) {
+	idx_t MaxThreads() const override {
+		return work.size() - 2;
+	}
+
+private:
+	std::mutex mutex_;
+	std::vector<discreture::Permutations<int>::iterator> work;
+	const std::vector<discreture::Permutations<int>::iterator> stops;
+	idx_t current_work_idx;
+	double progress;
+
+public:
+	explicit PermGlobalState(int n_val, int threads)
+	    : perm(n_val), n(n_val), completed(0),
+	      work(discreture::divide_work_in_equal_parts(perm.begin(), perm.end(), threads)),
+	      stops(discreture::divide_work_in_equal_parts(perm.begin(), perm.end(), threads)), current_work_idx(0),
+	      progress(0.0) {
+	}
+
+	boost::optional<idx_t> GetWorkIdx() {
+		std::lock_guard<std::mutex> lock(mutex_);
+		auto result = current_work_idx++;
+		if (result > work.size() - 2) {
+			return boost::none;
+		}
+		return result;
+	}
+
+	discreture::Permutations<int>::iterator &GetIter(idx_t idx) {
+		return work[idx];
+	}
+
+	const discreture::Permutations<int>::iterator &GetStop(idx_t idx) {
+		return stops[idx];
+	}
+
+	void IncrementCompleted() {
+		completed.fetch_add(1, std::memory_order_relaxed);
+		progress = (completed.load() / (double)(work.size() - 1)) * 100.0;
+	}
+
+	double Progress() const {
+		return progress;
+	}
+
+	inline bool IsLastIndex(idx_t idx) {
+		return idx >= work.size() - 2;
+	}
+};
+
+struct PermLocalState : public LocalTableFunctionState {
+	boost::optional<idx_t> work_idx;
+	bool is_last;
+
+	PermLocalState() {
+		work_idx = boost::none;
+		is_last = false;
 	}
 };
 
@@ -153,7 +211,21 @@ static unique_ptr<FunctionData> PermBind(ClientContext &context, TableFunctionBi
 		names.push_back("col" + std::to_string(i));
 	}
 
-	return make_uniq<PermBindData>(n);
+	return make_uniq<PermBindData>(n, false);
+}
+
+static unique_ptr<FunctionData> PermBindArray(ClientContext &context, TableFunctionBindInput &input,
+                                              vector<LogicalType> &return_types, vector<string> &names) {
+	int n = input.inputs[0].GetValue<int32_t>();
+
+	if (n <= 0) {
+		throw BinderException("Invalid argument for permutations: require n > 0");
+	}
+
+	names.push_back("permutation");
+	return_types.push_back(LogicalType::ARRAY(LogicalType::INTEGER, n));
+
+	return make_uniq<PermBindData>(n, true);
 }
 
 // ----------- Init Global State -----------
@@ -171,7 +243,12 @@ unique_ptr<LocalTableFunctionState> CombInitLocal(ExecutionContext &context, Tab
 
 static unique_ptr<GlobalTableFunctionState> PermInitGlobal(ClientContext &context, TableFunctionInitInput &input) {
 	auto &bind_data = input.bind_data->Cast<PermBindData>();
-	return make_uniq<PermGlobalState>(bind_data.n);
+	return make_uniq<PermGlobalState>(bind_data.n, context.db->config.options.maximum_threads * 50);
+}
+
+unique_ptr<LocalTableFunctionState> PermInitLocal(ExecutionContext &context, TableFunctionInitInput &input,
+                                                  GlobalTableFunctionState *global_state) {
+	return make_uniq<PermLocalState>();
 }
 
 // ----------- Execution Functions -----------
@@ -254,25 +331,82 @@ static void CombExec(ClientContext &context, TableFunctionInput &data, DataChunk
 }
 
 static void PermExec(ClientContext &context, TableFunctionInput &data, DataChunk &output) {
-	auto &state = data.global_state->Cast<PermGlobalState>();
-	idx_t count = 0;
+	auto &bind_data = data.bind_data->Cast<PermBindData>();
+	auto &global_state = data.global_state->Cast<PermGlobalState>();
+	auto &local_state = data.local_state->Cast<PermLocalState>();
 
 	auto output_vectors = output.data;
 
-	// I want to get these pointers for all contents of the output columns
-	auto data_ptrs = vector<int32_t *>(state.n);
-	for (idx_t col = 0; col < state.n; col++) {
-		data_ptrs[col] = FlatVector::GetData<int32_t>(output_vectors[col]);
+	duckdb::vector<int32_t *> output_column_ptrs;
+	int32_t *data_ptr = nullptr;
+	if (!bind_data.return_array) {
+		// I want to get these pointers for all contents of the output columns
+		output_column_ptrs.reserve(global_state.n);
+		for (idx_t col = 0; col < global_state.n; col++) {
+			output_column_ptrs.push_back(FlatVector::GetData<int32_t>(output_vectors[col]));
+		}
+	} else {
+		auto &result_data_children = ArrayVector::GetEntry(output.data[0]);
+		data_ptr = FlatVector::GetData<int32_t>(result_data_children);
 	}
 
-	while (state.it != state.perm.end() && count < STANDARD_VECTOR_SIZE) {
-		for (idx_t col = 0; col < state.n; col++) {
-			data_ptrs[col][count] = (*state.it)[col];
+	idx_t count = 0;
+	const auto n = global_state.n;
+	const auto n_size = sizeof(int32_t) * n;
+
+	while (count < STANDARD_VECTOR_SIZE) {
+		if (!local_state.work_idx.has_value()) {
+			// Lock the mutex in the global state
+			local_state.work_idx = global_state.GetWorkIdx();
+			if (!local_state.work_idx.has_value()) {
+				output.SetCardinality(count);
+				return;
+			}
+			local_state.is_last = global_state.IsLastIndex(*local_state.work_idx);
 		}
-		++state.it;
-		count++;
+
+		auto &idx = *local_state.work_idx;
+
+		auto &iterator = global_state.GetIter(idx);
+		auto &end = global_state.GetStop(idx + 1);
+
+		if (local_state.is_last) {
+			while (count < STANDARD_VECTOR_SIZE && iterator != global_state.perm.end()) {
+				const auto &perm = *iterator;
+				if (!bind_data.return_array) {
+					for (idx_t col = 0; col < n; col++) {
+						output_column_ptrs[col][count] = perm[col];
+					}
+				} else {
+					memcpy(&data_ptr[count * n], &perm[0], n_size);
+				}
+				++iterator;
+				count++;
+			}
+			if (iterator == global_state.perm.end()) {
+				local_state.work_idx = boost::none;
+				global_state.IncrementCompleted();
+			}
+		} else {
+			while (count < STANDARD_VECTOR_SIZE && iterator != end) {
+				const auto &perm = *iterator;
+				if (!bind_data.return_array) {
+					for (idx_t col = 0; col < n; col++) {
+						output_column_ptrs[col][count] = perm[col];
+					}
+				} else {
+					memcpy(&data_ptr[count * n], &perm[0], n_size);
+				}
+				++iterator;
+				count++;
+			}
+			if (iterator == end) {
+				local_state.work_idx = boost::none;
+				global_state.IncrementCompleted();
+			}
+		}
+		output.SetCardinality(count);
 	}
-	output.SetCardinality(count);
 }
 
 unique_ptr<NodeStatistics> CombCardinality(ClientContext &context, const FunctionData *bind_data) {
@@ -317,6 +451,13 @@ unique_ptr<BaseStatistics> PermStatistics(ClientContext &context, const Function
 	return r.ToUnique();
 }
 
+double PermScanProgress(ClientContext &context, const FunctionData *bind_data,
+                        const GlobalTableFunctionState *global_state) {
+	auto &state = global_state->Cast<PermGlobalState>();
+
+	return state.Progress();
+}
+
 double CombScanProgress(ClientContext &context, const FunctionData *bind_data,
                         const GlobalTableFunctionState *global_state) {
 	auto &state = global_state->Cast<CombGlobalState>();
@@ -339,13 +480,22 @@ static void LoadInternal(DatabaseInstance &instance) {
 	//	combinations_func_array.statistics = CombStatistics;
 	combinations_func_array.table_scan_progress = CombScanProgress;
 
-	TableFunction permutations_func("permutations", {LogicalType::INTEGER}, PermExec, PermBind, PermInitGlobal);
+	TableFunction permutations_func("permutations", {LogicalType::INTEGER}, PermExec, PermBind, PermInitGlobal,
+	                                PermInitLocal);
 	permutations_func.cardinality = PermCardinality;
 	permutations_func.statistics = PermStatistics;
+	permutations_func.table_scan_progress = PermScanProgress;
+
+	TableFunction permutations_func_array("permutations_array", {LogicalType::INTEGER}, PermExec, PermBindArray,
+	                                      PermInitGlobal, PermInitLocal);
+	permutations_func_array.cardinality = PermCardinality;
+	//	permutations_func_array.statistics = PermStatistics;
+	permutations_func_array.table_scan_progress = PermScanProgress;
 
 	ExtensionUtil::RegisterFunction(instance, combinations_func);
 	ExtensionUtil::RegisterFunction(instance, combinations_func_array);
 	ExtensionUtil::RegisterFunction(instance, permutations_func);
+	ExtensionUtil::RegisterFunction(instance, permutations_func_array);
 }
 
 void DiscretureExtension::Load(DuckDB &db) {
